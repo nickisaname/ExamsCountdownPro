8f16707070fbd64edd2ffc120449696110ba9b14
alessandro <alessandro.medina@outlook.it>
2020-05-21 18:16:04 +0200

    Commit Medina 2

--- app/src/main/java/pdm/uninsubria/it/examscountdownpro/MainActivity.kt
+++ app/src/main/java/pdm/uninsubria/it/examscountdownpro/MainActivity.kt
@@ -5,11 +5,11 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
-
+    //Commento di prova 2 @medinaalessandro
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        //commut
         //Commento di prova @medinaalessandro
     }
 }

aaab9034597e9ceb889e729d9cb86d772d4bdcec
Nicolas <nick.mele.96@gmail.com>
2020-05-21 18:15:51 +0200

    Commento di prova @nickisaname

--- app/src/main/java/pdm/uninsubria/it/examscountdownpro/MainActivity.kt
+++ app/src/main/java/pdm/uninsubria/it/examscountdownpro/MainActivity.kt
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
-
+//Commento di prova @nickisaname
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)

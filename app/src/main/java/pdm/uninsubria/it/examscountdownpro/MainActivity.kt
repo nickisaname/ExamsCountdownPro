6c90a5c3c98fcc58739072adb72cd8b0e2ca1b6a
Nicolas <nick.mele.96@gmail.com>
2020-05-21 20:49:03 +0200

    Added the "add" button to the toolbar

--- app/src/main/java/pdm/uninsubria/it/examscountdownpro/MainActivity.kt
+++ app/src/main/java/pdm/uninsubria/it/examscountdownpro/MainActivity.kt
@@ -2,8 +2,14 @@ package pdm.uninsubria.it.examscountdownpro
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 
 class MainActivity : AppCompatActivity() {
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.add_button, menu)
+        return true
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)

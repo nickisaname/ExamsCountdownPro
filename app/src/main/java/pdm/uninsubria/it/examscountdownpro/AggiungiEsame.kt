538e5ec16a66a8bde855d02fbdcf771e41f1768d
alessandro <alessandro.medina@outlook.it>
2020-05-21 22:01:02 +0200

    - Second activity created  - This'll be used for creating new exams

---
+++ app/src/main/java/pdm/uninsubria/it/examscountdownpro/AggiungiEsame.kt
@@ -0,0 +1,12 @@
+package pdm.uninsubria.it.examscountdownpro
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AggiungiEsame : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_aggiungi_esame)
+    }
+}
--- app/src/main/java/pdm/uninsubria/it/examscountdownpro/MainActivity.kt
+++ app/src/main/java/pdm/uninsubria/it/examscountdownpro/MainActivity.kt
@@ -5,16 +5,16 @@ import android.os.Bundle
 import android.view.Menu
 
 class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         menuInflater.inflate(R.menu.add_button, menu)
         return true
     }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
 
-    }
 }

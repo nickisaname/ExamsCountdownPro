ca736747c2ac22a9aeb86b7af7e213aec5e18cc1
Alessandro <alessandro.medina@outlook.it>
2020-05-21 22:44:08 +0200

    - Creation editTexts for:    - Exam name    - Exam date    - Exam Description

--- app/src/main/java/pdm/uninsubria/it/examscountdownpro/AggiungiEsame.kt
+++ app/src/main/java/pdm/uninsubria/it/examscountdownpro/AggiungiEsame.kt
@@ -9,6 +9,8 @@ class AggiungiEsame : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_aggiungi_esame)
+
+        supportActionBar?.title = "Aggiungi un nuovo esame"
     }
 
 }

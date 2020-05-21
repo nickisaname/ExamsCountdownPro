2d8ec5f8590f24fdfefd87bab07aee91ee9b2aa2
Nicolas <nick.mele.96@gmail.com>
2020-05-21 18:39:21 +0200

    Rimossi i commenti di prova

--- app/src/main/java/pdm/uninsubria/it/examscountdownpro/MainActivity.kt
+++ app/src/main/java/pdm/uninsubria/it/examscountdownpro/MainActivity.kt
@@ -1,17 +1,13 @@
-//Commento di prova @nickisaname
 package pdm.uninsubria.it.examscountdownpro
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
-    //Commento di prova 2 @medinaalessandro
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        //commut
-        //Commento di prova @medinaalessandro
+
     }
 }
-
-//commento

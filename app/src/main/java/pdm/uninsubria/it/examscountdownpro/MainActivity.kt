95a7d8d6ed3f665aa156a529b30047bf331869e3
Nicolas <nick.mele.96@gmail.com>
2020-05-21 22:20:21 +0200

    Added the second activity and tested if it worked with the button "add"

--- app/src/main/java/pdm/uninsubria/it/examscountdownpro/MainActivity.kt
+++ app/src/main/java/pdm/uninsubria/it/examscountdownpro/MainActivity.kt
@@ -3,8 +3,10 @@ package pdm.uninsubria.it.examscountdownpro
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuItem
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -15,6 +17,18 @@ class MainActivity : AppCompatActivity() {
         return true
     }
 
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_add -> {
+                aggiungiEsame()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
 
+    fun aggiungiEsame () {
+        setContentView(R.layout.activity_aggiungi_esame)
+    }
 
 }

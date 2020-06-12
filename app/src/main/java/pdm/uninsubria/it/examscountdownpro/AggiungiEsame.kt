04de0b726b80c4ac9f611f04dec5e9668e9d2da3
Alessandro <alessandro.medina@outlook.it>
2020-06-12 23:33:46 +0200

    - Add modify to the main_row.xml
    Signed-off-by: alessandro <alessandro.medina@outlook.it>

--- app/src/main/java/pdm/uninsubria/it/examscountdownpro/AggiungiEsame.kt
+++ app/src/main/java/pdm/uninsubria/it/examscountdownpro/AggiungiEsame.kt
@@ -6,12 +6,14 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_aggiungi_esame.*
+import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
-import android.widget.Button
 
 class AggiungiEsame : AppCompatActivity() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,9 +21,7 @@ class AggiungiEsame : AppCompatActivity() {
 
         //Titolo sulla toolbar
         supportActionBar?.title = "Aggiungi nuovo esame"
-
     }
-
 
     //Settaggio per mostrare l'incona Annulla per tornare indietro
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -40,7 +40,6 @@ class AggiungiEsame : AppCompatActivity() {
     }
 
      fun openCalendar (view: View) {
-
         val calendar= Calendar.getInstance()
         val year = calendar.get(Calendar.YEAR)
         val month = calendar.get(Calendar.MONTH)
@@ -48,23 +47,10 @@ class AggiungiEsame : AppCompatActivity() {
 
         val datePickerDialog = DatePickerDialog(this, DatePickerDialog.OnDateSetListener
         { view, year, monthOfYear, dayOfMonth ->
-
             editText5.setText("" + dayOfMonth + " - " + (monthOfYear+1) + " - " + year)
-
         }, year, month, day)
-
          datePickerDialog.show()
-
     }
-
-    fun AggiungiItem(view: View){
-        val bt : Button = findViewById(R.id.buttonAdd)
-        bt.setOnClickListener(){
-            val it = Intent(this, MainActivity::class.java)
-            startActivity(it)
-        }
-    }
-
 }
 
 
--- app/src/main/java/pdm/uninsubria/it/examscountdownpro/MainActivity.kt
+++ app/src/main/java/pdm/uninsubria/it/examscountdownpro/MainActivity.kt
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.ArrayAdapter
+import android.widget.ListView
 
 class MainActivity : AppCompatActivity() {
 
